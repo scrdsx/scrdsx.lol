4e7d8d5c3a742cebe43e1cf37d49a18d81d9d8b8
scrdsx <138362343+scrdsx@users.noreply.github.com>
2024-06-09 17:16:08 +0200

    Add files via upload

---
+++ scrdsx.lol/pimp/script.js
@@ -0,0 +1,44 @@
+const textArray = [
+    "I am Todd/Heresys",
+    "Loved by scrdsx",
+    "scrdsx.lol/pimp"
+];
+let textIndex = 0;
+let charIndex = 0;
+const typedTextElement = document.getElementById("typedText");
+
+typedTextElement.style.height = typedTextElement.clientHeight + "px";
+
+function type() {
+    const currentText = textArray[textIndex];
+    if (charIndex < currentText.length) {
+        typedTextElement.textContent += currentText.charAt(charIndex);
+        charIndex++;
+        setTimeout(type, 50);
+    } else {
+        setTimeout(erase, 1000);
+    }
+}
+
+function erase() {
+    const currentText = textArray[textIndex];
+    if (charIndex > 0) {
+        typedTextElement.textContent = currentText.substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, 50);
+    } else {
+        textIndex = (textIndex + 1) % textArray.length;
+        typedTextElement.textContent = '';
+        setTimeout(type, 1000);
+    }
+}
+
+type();
+
+const overlay = document.getElementById("overlay");
+const backgroundVideo = document.getElementById("background-video");
+
+overlay.addEventListener("click", () => {
+    overlay.style.display = "none";
+    backgroundVideo.play();
+});
